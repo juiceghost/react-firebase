adab6390d42c9076feb042656f61844e01d5a971
juiceghost <jg@renegade.se>
2021-03-08 10:06:52 +0100

    Refactoring withAuthorization almost done

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,20 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import { PasswordForgetForm } from '../PasswordForget';
 import PasswordChangeForm from '../PasswordChange';
 import { AuthUserContext, withAuthorization } from '../Session';
 
-const AccountPage = () => (
-    <AuthUserContext.Consumer>
-        {authUser => (
-            <div>
-                <h1>Account: {authUser.email}</h1>
-                <PasswordForgetForm />
-                <PasswordChangeForm />
-            </div>
-        )}
-    </AuthUserContext.Consumer>
-);
+const AccountPage = () => {
+    const { email } = useContext(AuthUserContext);
+
+    return (
+        <div>
+            <h1>Account: {email}</h1>
+            <PasswordForgetForm />
+            <PasswordChangeForm />
+        </div>
+    );
+}
+
 
 const condition = authUser => !!authUser;
 
